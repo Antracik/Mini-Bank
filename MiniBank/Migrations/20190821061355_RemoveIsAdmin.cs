813e45d4d5dd7e45c6d91ff0bfe6ee232e301937
Preslav <preslav.panayotov@mypos.com>
2019-08-21 09:15:47 +0300

    Removed IsAdmin column from the db

---
+++ MiniBank/Migrations/20190821061355_RemoveIsAdmin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mini_Bank.Migrations
+{
+    public partial class RemoveIsAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
